7f1e8a105f8a0ee1381e6e0843a60650852d913c
Sebastián Dosman <jsdosman0@gmail.com>
2024-04-21 20:12:04 -0500

    Refactor: some of code refactored

--- src/dto/room/create-room.dto.ts
+++ src/dto/room/create-room.dto.ts
@@ -1,4 +1,5 @@
-import Joi from "joi";
+import Joi from 'joi';
+
 
 export const roomCreateSchema = Joi.object({
     name: Joi.string()
@@ -13,16 +14,16 @@ export const roomCreateSchema = Joi.object({
         }),
     state: Joi.string()
         .required()
-        .valid('Sin Iniciar', 'En Curso', 'Finalizado')
+        .valid('Sin iniciar', 'En curso', 'Finalizado')
         .messages({
             'string.empty': 'Room state cannot be empty.',
-            'any.only': 'Room state must be either "Sin Iniciar", "En Curso" or "Finalizado".',
+            'any.only': 'Room state must be either "Sin iniciar", "En curso" or "Finalizado".',
             'any.required': 'Room state is required.'
         }),
     idCategory: Joi.string()
         .required()
         .messages({
-            'string.empty': 'Category ID cannot be empty.',
-            'any.required': 'Category ID is required.'
+            'string.empty': 'Category id cannot be empty.',
+            'any.required': 'Category id is required.'
         })
 }).options({ abortEarly: false });
