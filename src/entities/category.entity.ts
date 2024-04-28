d1644e39525c92d412721a8c125d79b23323b3fc
Juan Sebastián Dosman <jsdosman0@gmail.com>
2024-04-27 22:33:56 -0500

    Update category.entity.ts

--- src/entities/category.entity.ts
+++ src/entities/category.entity.ts
@@ -3,7 +3,7 @@ import { Word } from './word.entity';
 
 
 @Entity({ name: 'category'})
-export class Category extends BaseEntity{
+export class Category extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
